3e62efcf58936e85ee31861f6bd808fa42b86478
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2022-10-17 16:50:50 -0300

    criado menu

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,12 @@
+const Menu = () => {
+  return (
+    <menu className="menu">
+      <h1 className="menu__title">Rockr Blog</h1>
+      <div className="menu__links">
+        <li className="menu__link">Posts</li>
+        <li className="menu__link">Contact</li>
+      </div>
+    </menu>
+  );
+};
+export default Menu;
