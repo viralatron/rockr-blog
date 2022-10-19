cd7b7eab4566d8a01ec7b476e98affae243f0cff
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2022-10-19 14:33:48 -0300

    criado infinite scroll
    criado infinite scroll
    criada maneira de diagramar os artigos

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,28 +1,73 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import BlogPost from "./BlogPost";
 
 const Blog = () => {
   const baseUrl = "https://stormy-shelf-93141.herokuapp.com/";
-  const artLimit = 11;
+  const artLimit = 3;
 
-  const [blogPosts, setPosts] = useState();
-  const fetchData = async (page, limit) => {
+  const [blogPosts, setPosts] = useState({
+    loading: false,
+    posts: [],
+    page: 0,
+    tick: 0,
+  });
+
+  const loadingRef = useRef(null);
+  const fetchData = async (page) => {
+    setPosts((blogPosts) => ({ ...blogPosts, loading: true }));
     const getData = await fetch(
-      `${baseUrl}articles?_page=${page}&_limit=${limit}`
+      `${baseUrl}articles?_page=${page}&_limit=${artLimit}`
     );
     const response = await getData.json();
-    setPosts(response);
-    console.log(blogPosts);
+    setPosts((blogPosts) => {
+      const posts = [];
+      posts.push(...blogPosts.posts);
+      posts.push(...response);
+      return {
+        ...blogPosts,
+        posts: posts,
+      };
+    });
+
+    setPosts((blogPosts) => ({ ...blogPosts, loading: false }));
+  };
+
+  const handleObserver = (entities) => {
+    const target = entities[0];
+
+    if (target.isIntersecting && !blogPosts.loading) {
+      fetchData(blogPosts.page);
+      const nextPage = blogPosts.page + 1;
+      setPosts({ ...blogPosts, page: nextPage });
+    }
+  };
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1.0,
   };
 
   useEffect(() => {
-    fetchData(0, artLimit);
-  }, []);
+    const observer = new IntersectionObserver(handleObserver, options);
+    if (loadingRef.current) observer.observe(loadingRef.current);
+    return () => {
+      if (loadingRef.current) observer.unobserve(loadingRef.current);
+    };
+  }, [options, loadingRef]);
 
   return (
     <main className="blog">
       {blogPosts &&
-        Object.keys(blogPosts).map((key) => <BlogPost {...blogPosts[key]} />)}
+        Object.keys(blogPosts.posts).map((key) => (
+          <BlogPost
+            itemNmbr={Number(key)}
+            key={blogPosts.posts[key].id}
+            {...blogPosts.posts[key]}
+          />
+        ))}
+      <div ref={loadingRef}>
+        <span>Carregando...</span>
+      </div>
     </main>
   );
 };
--- src/Components/BlogPost.jsx
+++ src/Components/BlogPost.jsx
@@ -6,17 +6,24 @@ const BlogPost = ({
   article,
   date,
   imageUrl,
+  itemNmbr,
 }) => {
   const headlineText =
     article.replace(/(<([^>]+)>)/gi, "").slice(0, 121) + " ...";
   const headlineTitle = title.slice(0, 21) + "...";
+  let postClass = "post";
+
+  if ((itemNmbr + 1) % 3 === 0) {
+    postClass += " post--big";
+  }
+  if ((itemNmbr - (itemNmbr % 3)) % 2 === 1) {
+    postClass += " post--reverse";
+  }
   return (
-    <div className="post" key={id}>
+    <div className={postClass} key={id}>
       <img src={imageUrl} alt={title} className="post__image" />
       <div className="details">
-        <a href={`mailto:${authorEmail}`} className="details__author">
-          {author}
-        </a>
+        <small className="details__author">{author}</small>
         <h2 className="details__title">{headlineTitle}</h2>
         <p className="details__headtext">{headlineText}</p>
         <img
